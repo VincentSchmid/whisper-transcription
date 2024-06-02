3a3dfd00a5fe6cdbbc7f57c89c0777bde8614787
Vincent <puterich@gmail.com>
2024-06-02 19:34:44 +0200

    add pipeline

--- go.mod
+++ go.mod
@@ -5,6 +5,13 @@ go 1.22.0
 require (
 	github.com/martinlindhe/subtitles v0.0.0-20210301100346-9aa635346c86
 	github.com/sashabaranov/go-openai v1.24.0
+	github.com/u2takey/ffmpeg-go v0.5.0
+)
+
+require (
+	github.com/aws/aws-sdk-go v1.53.10 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/u2takey/go-utils v0.3.1 // indirect
 )
 
 require (
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -10,22 +11,32 @@ import (
 	"github.com/joho/godotenv"
 	subtitles "github.com/martinlindhe/subtitles"
 	openai "github.com/sashabaranov/go-openai"
+	ffmpeg_go "github.com/u2takey/ffmpeg-go"
 )
 
 const (
-	inputDir         = "./test/"
-	outputDir        = "./output/"
-	transcribePrompt = "Es chömed Jugendlichi vor allem wo Risikoverhalte zeige, meischtens suizidali Jugendlichi, selte au Jugendlichi wo anderi bedrohe oder dor ihres Verhalte sich und anderi bedrohe. Do chas ned warte, die müend sofort igschätzt werde, wenn das kombiniert isch mit ere psychiatrische Uffälligkei. Aber es git au Störigsbilder wie Essstörige, schwerschti Depressione, manische Zustandsbilder oder Psychose, wo zwar au ned suizidal si müend, aber die chönnd au ned warte uf e reguläre Termin, au die chömed zu mir."
-	chatGptPrompt    = "Übersetze die Text wort für wort is schwiizerdütsch. Lahne d'timestamps bestah."
+	dataDir   = "./data"
+	outputDir = "./output"
+
+	videoDir               = dataDir + "/1-video"
+	audioDir               = dataDir + "/2-audio"
+	transcriptionDir       = dataDir + "/3-transcription"
+	gptResultDir           = dataDir + "/4-gpt_result"
+	transcribePrompt       = "Es chömed Jugendlichi vor allem wo Risikoverhalte zeige, meischtens suizidali Jugendlichi, selte au Jugendlichi wo anderi bedrohe oder dor ihres Verhalte sich und anderi bedrohe. Do chas ned warte, die müend sofort igschätzt werde, wenn das kombiniert isch mit ere psychiatrische Uffälligkei. Aber es git au Störigsbilder wie Essstörige, schwerschti Depressione, manische Zustandsbilder oder Psychose, wo zwar au ned suizidal si müend, aber die chönnd au ned warte uf e reguläre Termin, au die chömed zu mir."
+	chatGptPrompt          = "Übersetze die Text wort für wort is schwiizerdütsch. Lahne d'timestamps bestah."
+	transcriptionSuffix    = "_condensed"
 )
 
 func main() {
-	err := godotenv.Load()
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
 
 	openaiKey := os.Getenv("OPENAI_API_KEY")
+	if openaiKey == "" {
+		log.Fatalf("OPENAI_API_KEY is not set")
+	}
 
 	if err := os.MkdirAll(outputDir, os.ModePerm); err != nil {
 		log.Fatalf("Failed to create output directory: %v", err)
@@ -33,54 +44,156 @@ func main() {
 
 	openaiClient := openai.NewClient(openaiKey)
 
-	err = filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			log.Println(err)
-			return err
-		}
+	createFolders()
 
+	err = convertVideoFilesToAudio(videoDir, audioDir)
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = transcribeAudioFiles(openaiClient, audioDir, transcriptionSuffix, transcriptionDir)
+	if err != nil {
+		log.Println(err)
+	}
+
+	err = translateTranscriptions(openaiClient, transcriptionDir, transcriptionSuffix, gptResultDir)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Finished")
+}
+
+func createFolders() {
+	log.Println("Creating data directories...")
+	if err := os.MkdirAll(filepath.Join(videoDir), os.ModePerm); err != nil {
+		log.Fatalf("Failed to create video directory: %v", err)
+	}
+
+	if err := os.MkdirAll(filepath.Join(audioDir), os.ModePerm); err != nil {
+		log.Fatalf("Failed to create audio directory: %v", err)
+	}
+
+	if err := os.MkdirAll(filepath.Join(transcriptionDir), os.ModePerm); err != nil {
+		log.Fatalf("Failed to create transcription directory: %v", err)
+	}
+
+	if err := os.MkdirAll(filepath.Join(gptResultDir), os.ModePerm); err != nil {
+		log.Fatalf("Failed to create GPT result directory: %v", err)
+	}
+}
+
+func fileExists(parentFolderDir string, fileName string) bool {
+	// Create the full file path by joining the parent folder directory and the file name
+	fullFilePath := filepath.Join(parentFolderDir, fileName)
+
+	// Use os.Stat to get the file info
+	_, err := os.Stat(fullFilePath)
+
+	// If os.Stat returns an error and the error is of type os.ErrNotExist, the file does not exist
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	// If there's no error, or an error other than os.ErrNotExist, we assume the file exists
+	return err == nil
+}
+
+func convertVideoFilesToAudio(inputDir string, outputDir string) error {
+	return filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			baseName := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+			audioFilePath := filepath.Join(outputDir, baseName+".mp3")
 
-			log.Printf("Transcribing %s\n", path)
-			transcription := transcribe(openaiClient, transcribePrompt, path)
-
-			log.Printf("Writing transcription\n")
-
-			transcribeFilePath := filepath.Join(outputDir, "transkribiert_"+baseName+".srt")
-
-			writeFile(transcribeFilePath, transcription)
-			log.Printf("Transcription written to %s\n", transcribeFilePath)
-
-			log.Printf("Condensing transcription timestamps\n")
-			subs, err := subtitles.NewFromVTT(transcription)
-			if err != nil {
-				log.Printf("Error reading VTT file: %v\n", err)
-				return err
+			if !fileExists(outputDir, baseName+".mp3") {
+				log.Printf("Converting %s to audio...\n", path)
+				convertVideoToAudio(path, audioFilePath, 128)
+			} else {
+				log.Printf("File %s already exists\n", audioFilePath)
 			}
-
-			condensedTranscript := concatSubs(subs, 30)
-
-			log.Printf("Sending to GPT\n")
-			gptResult, err := chatGpt(openaiClient, chatGptPrompt, condensedTranscript.AsSRT())
-			if err != nil {
-				log.Printf("ChatGPT error: %v\n", err)
-				return err
-			}
-
-			log.Printf("Writing GPT result\n")
-
-			gptResultFilePath := filepath.Join(outputDir, "schweizerdeutsch_"+baseName+".txt")
-
-			writeFile(gptResultFilePath, gptResult)
-			log.Printf("GPT result written to %s\n", gptResultFilePath)
 		}
 
 		return nil
 	})
-	if err != nil {
-		log.Println(err)
-	}
+}
+
+func transcribeAudioFiles(openaiClient *openai.Client, inputDir string, condesedSuffix string, outputDir string) error {
+	return filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
+
+		if !info.IsDir() {
+			baseName := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+			transcriptionPath := filepath.Join(outputDir, baseName+".vtt")
+			condensedTranscriptionPath := filepath.Join(outputDir, baseName + condesedSuffix + ".vtt")
+
+			if !fileExists(outputDir, baseName + condesedSuffix + ".vtt") {
+				log.Printf("Transcribing audio file %s...\n", path)
+				transcription := transcribe(openaiClient, transcribePrompt, path)
+
+				log.Printf("Writing transcription to %s...\n", transcriptionPath)
+				writeFile(transcriptionPath, transcription)
+
+				log.Println("Condensing transcription timestamps...")
+				subs, err := subtitles.NewFromVTT(transcription)
+				if err != nil {
+					return fmt.Errorf("error reading SRT file: %w", err)
+				}
+
+				condensedTranscript := concatSubs(subs, 30)
+
+				log.Printf("Writing condensed transcription to %s...\n", condensedTranscriptionPath)
+				writeFile(condensedTranscriptionPath, condensedTranscript.AsSRT())
+			} else {
+				log.Printf("File %s already exists\n", condensedTranscriptionPath)
+			}
+		}
+
+		return nil
+	})
+}
+
+func translateTranscriptions(openaiClient *openai.Client, inputDir string, fileSuffix string, outputDir string) error {
+	return filepath.Walk(inputDir, func(path string, info os.FileInfo, err error) error {
+
+		if !info.IsDir() {
+			baseName := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
+			resultPath := filepath.Join(outputDir, baseName+".txt")
+
+			if strings.HasSuffix(baseName, fileSuffix) {
+
+			if !fileExists(outputDir, baseName+".txt") {
+				log.Printf("Reading Transcription from %s...\n", path)
+				transcriptionBytes, err := os.ReadFile(path)
+
+				if err != nil {
+					return fmt.Errorf("error reading file %s: %w", path, err)
+				}
+
+				log.Println("Translating with chat gpt...")
+				translatedResult, err := chatGpt(openaiClient, chatGptPrompt, string(transcriptionBytes))
+
+				if err != nil {
+					return fmt.Errorf("error translating file %s: %w", path, err)
+				}
+
+				log.Printf("Writing chatGpt Response to %s...\n", resultPath)
+				writeFile(resultPath, translatedResult)
+
+			} else {
+				log.Printf("File %s already exists\n", resultPath)
+			}
+		}
+		}
+
+		return nil
+	})
+}
+
+func convertVideoToAudio(videoPath string, audioFilePath string, audioBitrate int) {
+	ffmpeg_go.Input(videoPath).
+		Output(audioFilePath, ffmpeg_go.KwArgs{"ac": 1, "audio_bitrate": audioBitrate}).
+		OverWriteOutput().
+		ErrorToStdOut().
+		Run()
 }
 
 // Hier wird die Transkription mit der Zeitangabe zusammengefasst, sodass pro Zeitangabe weniger genau angezeigt wird
